5373f2c9286ad2bb61097ff84d27f52d95f860dc
Cody <cody@localhost>
2017-03-08 16:29:51 -0700

    Starting form application

--- app.ts
+++ app.ts
@@ -1,1 +1,37 @@
-// your code goes here
+
+import { bootstrap } from "@angular/platform-browser-dynamic";
+import { Component } from "@angular/core";
+
+@Component({
+	selector: 'reddit',
+	template: `
+	<form class="ui large form segment">
+		<h3 class="ui header">Add a Link</h3>
+		
+		<div class="field">
+			<label for="title">Title:</label>
+			<input name="title" #newTitle />
+		</div> 
+		
+		<div class="field">
+			<label for="link">Link:</label>
+			<input name="link" #newLink />
+		</div>
+		
+		<button (click)="addArticle(newTitle, newLink)" class="ui positive right floated button">Submit link</button>
+	</form>
+	`
+})
+
+class RedditApp {
+	
+	constructor() {
+	}
+	
+	addArticle(newTitle: HTMLInputElement, newLink: HTMLInputElement): void {
+		console.log(`Adding article title: ${newTitle.value} and link: ${newLink.value}`);
+	}
+	
+}
+
+bootstrap(RedditApp);
